22f31345af38aee736068519f2e934d454bab72e
Ta Quang <danxawe@gmail.com>
2022-04-19 15:18:40 +0200

    changed rigidbody vector type to SDL_FPoint

--- include/headers/Rigidbody.h
+++ include/headers/Rigidbody.h
@@ -1,34 +1,37 @@
 #pragma once
 #include "Vector.h"
-
+#include <SDL2/SDL.h>
 
 typedef struct rigidbodyInstance RigidbodyInstance;
 
-
 typedef struct rigidbody
 {
-    void (*setMass)(void*self, float mass);
-    float (*getMass)(void*self);
+    void (*setMass)(void *self, float mass);
+    float (*getMass)(void *self);
 
-    void (*setFriction)(void*self, float fx, float fy);
-    void (*setFrictionX)(void*self, float fx);
-    void (*setFrictionY)(void*self, float fx);
+    // Friction getter/setter
+    void (*setFriction)(void *self, float fx, float fy);
+    void (*setFrictionX)(void *self, float fx);
+    void (*setFrictionY)(void *self, float fx);
+    SDL_FPoint (*getFriction)(void *self);
+    // Force Getter/seter
+    void (*setForce)(void *self, float fx, float fy);
+    void (*setForceX)(void *self, float fx);
+    void (*setForceY)(void *self, float fy);
+    SDL_FPoint (*getForce)(void *self);
+    // velocity getter/setter
+    void (*setVelocity)(void *self, float fx, float fy);
+    void (*setVelocityX)(void *self, float fx);
+    void (*setVelocityY)(void *self, float fy);
+    SDL_FPoint (*getVelocity)(void *self);
+    // position, accileration getter
+    SDL_FPoint (*getPosition)(void *self);
+    SDL_FPoint (*getAcceleration)(void *self);
 
-    void (*setForce)(void*self, float x, float y);
-    void (*setForceX)(void*self, float x);
-    void (*setForceY)(void*self, float y);
-
-    Vector *(*getPosition)(void*self);
-    float (*getPositionX)(void*self);
-    float (*getPositionY)(void*self);
-    Vector *(*getAcceleration)(void*self);
-    Vector *(*getVelocity)(void*self);
-
-    void (*update)(void*self, float dt);
+    void (*update)(void *self, float dt);
     void (*destroy)(void *self);
 
     RigidbodyInstance *instance;
 } Rigidbody;
 
-
-Rigidbody * newRigidBody();
+Rigidbody *newRigidBody();
--- src/RigidBody.c
+++ src/RigidBody.c
@@ -2,139 +2,128 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 struct rigidbodyInstance
 {
     float mass;
     float gravity;
 
-    Vector * friction;
-    Vector * force;
-    Vector * pos;
-    Vector * acc;
-    Vector * vel;
+    SDL_FPoint friction;
+    SDL_FPoint force;
+    SDL_FPoint pos;
+    SDL_FPoint acc;
+    SDL_FPoint vel;
 };
 
-void setMass(void*self, float mass)
+void setMass(void *self, float mass)
 {
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
     instance->mass = mass;
 }
-float getMass(void*self)
+float getMass(void *self)
 {
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    return instance->mass;
+    return ((Rigidbody *)self)->instance->mass;
+}
+void setFriction(void *self, float fx, float fy)
+{
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
+    instance->friction.x = fx;
+    instance->friction.y = fy;
+}
+void setFrictionX(void *self, float fx)
+{
+    ((Rigidbody *)self)->instance->friction.x = fx;
+}
+void setFrictionY(void *self, float fy)
+{
+    ((Rigidbody *)self)->instance->friction.y = fy;
+}
+SDL_FPoint getFriction(void *self)
+{
+    return ((Rigidbody *)self)->instance->friction;
+}
+void setForce(void *self, float fx, float fy)
+{
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
+    instance->force.x = fx;
+    instance->force.y = fy;
+}
+void setForceX(void *self, float fx)
+{
+    ((Rigidbody *)self)->instance->force.x = fx;
+}
+void setForceY(void *self, float fy)
+{
+    ((Rigidbody *)self)->instance->force.y = fy;
+}
+SDL_FPoint getForce(void *self)
+{
+    return ((Rigidbody *)self)->instance->force;
+}
+void setVelocity(void *self, float fx, float fy)
+{
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
+    instance->force.x = fx;
+    instance->force.y = fy;
+}
+void setVelocityX(void *self, float fx)
+{
+    ((Rigidbody *)self)->instance->force.x = fx;
+}
+void setVelocityY(void *self, float fy)
+{
+    ((Rigidbody *)self)->instance->force.y = fy;
+}
+SDL_FPoint getVelocity(void *self)
+{
+    return ((Rigidbody *)self)->instance->force;
+}
+SDL_FPoint getPosition(void *self)
+{
+    return ((Rigidbody *)self)->instance->pos;
+}
+SDL_FPoint getAcceleration(void *self)
+{
+    return ((Rigidbody *)self)->instance->acc;
 }
 
-void setFriction(void*self, float fx, float fy)
+void updateRigidBody(void *self, float dt)
 {
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->friction->set(instance->friction, fx, fy);
-}
-void setFrictionX(void*self, float fx)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->friction->setX(instance->friction, fx);
-}
-void setFrictionY(void*self, float fy)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->friction->setY(instance->friction, fy);
-}
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
 
-    
-void setForce(void*self, float x, float y)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->force->set(instance->force, x, y);
-}
-void setForceX(void*self, float x)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->force->setX(instance->force, x);
-}
-void setForceY(void*self, float y)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-    instance->force->setY(instance->force, y);
-}
+    dt = dt / 60;
 
-Vector *getPosition(void*self)
-{
-    return ((Rigidbody*)self)->instance->pos;
-}
-float getPositionX(void*self)
-{
-    Vector *pos = ((Rigidbody*)self)->instance->pos;
-    return pos->getX(pos);
-}
-float getPositionY(void*self)
-{
-    Vector *pos = ((Rigidbody*)self)->instance->pos;
-    return pos->getY(pos);
-}
-Vector *getAcceleration(void*self)
-{
-    return ((Rigidbody*)self)->instance->acc;
-}
-Vector *getVelocity(void*self)
-{
-    return ((Rigidbody*)self)->instance->vel;
-}
+    float frictionX = instance->acc.x;
+    float frictionY = instance->acc.y;
+    float forceX = instance->force.x;
+    float forceY = instance->force.y;
+    // acc
+    float accX = (forceX + frictionX) / instance->mass;
+    float accY = ((instance->gravity / instance->mass) + frictionY + forceY) / instance->mass;
+    instance->acc.x = accX;
+    instance->acc.y = accY;
+    // vel
+    float velX = instance->vel.x + accX * dt;
+    float velY = instance->vel.y + accY * dt;
+    instance->vel.x = velX;
+    instance->vel.y = velY;
+    // pos
+    float posX = velX * dt;
+    float posY = velY * dt;
+    instance->pos.x = posX;
+    instance->pos.y = posY;
 
-
-void updateRigidBody(void*self, float dt)
-{
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-
-    dt = dt/60;
-
-    float frictionX = instance->acc->getX(instance->acc);
-    float frictionY = instance->acc->getY(instance->acc);
-    float forceX = instance->force->getX(instance->force);
-    float forceY = instance->force->getY(instance->force);
-    //acc
-    float accX = (forceX + frictionX)/instance->mass;
-    float accY = ((instance->gravity/instance->mass) + frictionY + forceY)/instance->mass;
-    instance->acc->set(instance->acc, accX, accY);
-    //vel
-    float velX = instance->vel->getX(instance->vel) + accX*dt;
-    float velY = instance->vel->getY(instance->vel) + accY*dt;
-    instance->vel->set(instance->vel, velX, velY);
-    //pos
-    float posX = velX*dt;
-    float posY = velY*dt;
-    instance->pos->set(instance->pos, posX, posY);
-
-    printf("posX: %f, posY: %f\n",posX, posY);
-
+    // printf("posX: %f, posY: %f\n", posX, posY);
 }
 void destroyRigidBody(void *self)
 {
-    RigidbodyInstance *instance = ((Rigidbody*)self)->instance;
-
-    instance->friction->destroy(instance->friction);
-    free(instance->friction);
-
-    instance->pos->destroy(instance->pos);
-    free(instance->pos);
-
-    instance->force->destroy(instance->force);
-    free(instance->force);
-
-    instance->acc->destroy(instance->acc);
-    free(instance->acc);
-
-    instance->vel->destroy(instance->vel);
-    free(instance->vel);
+    RigidbodyInstance *instance = ((Rigidbody *)self)->instance;
 
     free(instance);
-
     free(self);
     printf("RigidBody destroyed\n");
 }
 
-Rigidbody * newRigidBody()
+Rigidbody *newRigidBody()
 {
     Rigidbody *self = malloc(sizeof(Rigidbody));
     self->instance = malloc(sizeof(RigidbodyInstance));
@@ -142,38 +131,29 @@ Rigidbody * newRigidBody()
     self->instance->mass = 1;
     self->instance->gravity = 5;
 
-    self->instance->friction = malloc(sizeof(Vector));
-    self->instance->friction = newVector();
-    self->instance->pos = malloc(sizeof(Vector));
-    self->instance->pos = newVector();
-    self->instance->force = malloc(sizeof(Vector));
-    self->instance->force = newVector();
-    self->instance->acc = malloc(sizeof(Vector));
-    self->instance->acc = newVector();
-    self->instance->vel = malloc(sizeof(Vector));
-    self->instance->vel = newVector();
-
     self->getMass = getMass;
     self->setMass = setMass;
 
     self->setFriction = setFriction;
     self->setFrictionX = setFrictionX;
     self->setFrictionY = setFrictionY;
+    self->getFriction = getFriction;
 
     self->setForce = setForce;
     self->setForceX = setForceX;
     self->setForceY = setForceY;
+    self->getForce = getForce;
 
-    self->getAcceleration = getAcceleration;
+    self->setVelocity = setForce;
+    self->setVelocityX = setForceX;
+    self->setVelocityY = setForceY;
+    self->getVelocity = getForce;
+
     self->getPosition = getPosition;
-    self->getPositionX = getPositionX;
-    self->getPositionY = getPositionY;
-    self->getVelocity = getVelocity;
+    self->getAcceleration = getAcceleration;
 
     self->update = updateRigidBody;
     self->destroy = destroyRigidBody;
 
     return self;
 }
-
-
