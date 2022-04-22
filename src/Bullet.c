b0bfd42ac2f02e182364a12a74c6eb3806ee2af8
tattack99 <62747925+tattack99@users.noreply.github.com>
2022-04-22 11:49:08 +0200

    bullet copy of cube

--- src/Bullet.c
+++ src/Bullet.c
@@ -1,4 +1,4 @@
-#include "Cube.h"
+#include "Bullet.h"
 #include <stdlib.h>
 #include "Transform.h"
 #include "SDL2/SDL.h"
@@ -7,16 +7,16 @@
 #include "CollisionManager.h"
 #include "SDL2/SDL.h"
 
-struct cubeInstance
+struct bulletInstance
 {
     Transform *position;
     SDL_FPoint vel;
     SDL_Rect hitBox;
 };
 
-void renderCube(void *self)
+void renderBullet(void *self)
 {
-    CubeInstance *instance = ((Cube *)self)->instance;
+    BulletInstance *instance = ((Bullet *)self)->instance;
     GameEngin *engin = getGameEngin();
 
     SDL_Rect box = {
@@ -30,9 +30,9 @@ void renderCube(void *self)
     SDL_Rect SRect = {100, 200, 100, 50};
     SDL_RenderDrawRect(engin->getRenderer(engin), &SRect);
 }
-void updateCube(void *self, float dt)
+void updateBullet(void *self, float dt)
 {
-    CubeInstance *instance = ((Cube *)self)->instance;
+    BulletInstance *instance = ((Bullet *)self)->instance;
 
     CollisionManager *collision = GetCollisionManager();
     SDL_Rect DRect = {
@@ -55,45 +55,45 @@ void updateCube(void *self, float dt)
 
     instance->position->translate(instance->position, instance->vel.x * dt, instance->vel.y * dt);
 }
-void eventCube(void *self)
+void eventBullet(void *self)
 {
     InputHandler *inputHandler = getInputHandler();
     int x, y;
     unsigned int buttons = inputHandler->getMouseState(&x, &y);
 
-    CubeInstance *instance = ((Cube *)self)->instance;
+    BulletInstance *instance = ((Bullet *)self)->instance;
 
-    if ((buttons & SDL_BUTTON_LMASK) != 0)
+    if ((buttons & SDL_BUTTON_RMASK) != 0)
     {
         instance->vel.x = (x - instance->position->getX(instance->position)) / 120;
         instance->vel.y = (y - instance->position->getY(instance->position)) / 120;
     }
 }
-void destroyCube(void *self)
+void destroyBullet(void *self)
 {
     free(self);
-    free(((Cube *)self)->instance);
+    free(((Bullet *)self)->instance);
 }
 
-Cube *newCube()
+Bullet *newBullet()
 {
-    Cube *self = malloc(sizeof(Cube));
-    self->instance = malloc(sizeof(CubeInstance));
+    Bullet *self = malloc(sizeof(Bullet));
+    self->instance = malloc(sizeof(BulletInstance));
 
     self->instance->position = newTransform();
 
     self->instance->vel.x = 1;
     self->instance->vel.y = 1;
 
-    self->instance->hitBox.x = 0;
-    self->instance->hitBox.y = 0;
+    self->instance->hitBox.x = 300;
+    self->instance->hitBox.y = 100;
     self->instance->hitBox.w = 25;
     self->instance->hitBox.h = 25;
 
-    self->render = renderCube;
-    self->update = updateCube;
-    self->events = eventCube;
-    self->destroy = destroyCube;
+    self->render = renderBullet;
+    self->update = updateBullet;
+    self->events = eventBullet;
+    self->destroy = destroyBullet;
 
     return self;
 }
--- src/GameEngin.c
+++ src/GameEngin.c
@@ -9,6 +9,7 @@
 #include "Warrior.h"
 #include "FpsManager.h"
 #include "Cube.h"
+#include "Bullet.h"
 #include "map.h"
 
 struct enginInstance
@@ -40,6 +41,9 @@ bool init(void *self, char *title, int width, int height, int fullScreen)
     Cube *cube = newCube();
     entityManager->add(entityManager, "cube-1", cube);
 
+    Bullet *bullet = newBullet();
+    entityManager->add(entityManager, "bullet-1", bullet);
+
     Engin->instance->isRunning = true;
 
 
@@ -57,6 +61,10 @@ void handleEvents(void *self)
     // cube
     Cube *cube = entityManager->getByID(entityManager, "cube-1");
     cube->events(cube);
+
+    // bullet
+    Bullet *bullet = entityManager->getByID(entityManager, "bullet-1");
+    bullet->events(bullet);
 }
 
 void handleUpdates(void *self)
@@ -73,6 +81,9 @@ void handleUpdates(void *self)
 
     Cube *cube = entityManager->getByID(entityManager, "cube-1");
     cube->update(cube, dt);
+
+    Bullet *bullet = entityManager->getByID(entityManager, "bullet-1");
+    bullet->update(bullet, dt);
 
     // updates functions go here !!!
 }
@@ -97,6 +108,12 @@ void handleRenders(void *self)
     SDL_SetRenderDrawColor(Engin->instance->renderer, 0, 0, 255, 255);
     Cube *cube = entityManager->getByID(entityManager, "cube-1");
     cube->render(cube);
+
+    // render bullet
+    SDL_SetRenderDrawColor(Engin->instance->renderer, 0, 0, 255, 0);
+    Bullet *bullet = entityManager->getByID(entityManager, "bullet-1");
+    bullet->render(bullet);
+    
 
     // render functions go here !!!
     SDL_RenderPresent(Engin->instance->renderer);
