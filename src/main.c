7f8709d1ce336e4e19147765167cb42007680bf5
tattack99 <62747925+tattack99@users.noreply.github.com>
2022-05-11 17:44:19 +0200

    good dev2

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
     if (!isInitSucceed)
         return 1;
 
-    network->connect(network, 2);
+    network->connect(network, 4);
 
     Engine->innitGameInstances(Engine);
 
